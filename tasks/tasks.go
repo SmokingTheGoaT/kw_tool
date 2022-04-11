d8a02e0b90d3dabd8aad29378983a16044cd4ac6
SmokingTheGoaT <orlando.heaply@gmail.com>
2022-04-11 13:56:59 -0700

    fixing import cycle error

--- tasks/tasks.go
+++ tasks/tasks.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"errors"
 	"github.com/hibiken/asynq"
-	"kw_tool/types/requests"
 	"time"
 )
 
@@ -13,16 +12,20 @@ const (
 	TypeRecursiveCrawlRequest = "crawl:recursive"
 )
 
+type suggestInterface interface {
+	Run() (err error)
+}
+
 type RecursiveCrawlPayload struct {
-	Request *requests.Suggest
+	Request interface{}
 }
 
 func NewRecursiveCrawlTask(req interface{}, d time.Duration) (*asynq.Task, error) {
 	var task *asynq.Task
 	var payload []byte
 	var err error
-	if rr, ok := req.(*requests.Suggest); ok {
-		if payload, err = json.Marshal(RecursiveCrawlPayload{Request: rr}); err != nil {
+	if _, ok := req.(suggestInterface); ok {
+		if payload, err = json.Marshal(RecursiveCrawlPayload{Request: req}); err != nil {
 			task = nil
 		} else {
 			task = asynq.NewTask(TypeRecursiveCrawlRequest, payload, asynq.MaxRetry(1), asynq.ProcessIn(d))
@@ -38,7 +41,7 @@ func HandleRecursiveCrawlTask(ctx context.Context, t *asynq.Task) error {
 	var p RecursiveCrawlPayload
 	var err error
 	if err = json.Unmarshal(t.Payload(), &p); err == nil {
-		err = p.Request.Run()
+		err = p.Request.(suggestInterface).Run()
 	}
 	return err
 }
